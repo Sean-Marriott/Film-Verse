bf430e6717efc36b5a8b8a26b74bc5cd628557dc
Sean Marriott <sma462@uclive.ac.nz>
2023-05-12 10:21:17 +1200

    Adding routes for home, search and all

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Search from "./components/Search";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <div className="App">
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/search" element={<Search/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </div>
+        </Router>
+    </div>
 );
