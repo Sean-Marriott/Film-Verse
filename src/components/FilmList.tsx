e0b4923e94995ca7492310597b03af1b381de665
Sean <sma462@uclive.ac.nz>
2023-05-20 12:21:38 +1200

    Added the ability to filter films by Genre

--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -1,23 +1,52 @@
 import axios from 'axios';
 import React, {useState} from "react";
-import {AlertTitle, Alert, Pagination} from "@mui/material";
+import {
+    AlertTitle,
+    Alert,
+    Pagination,
+    SelectChangeEvent,
+    FormControl,
+    InputLabel, Select, OutlinedInput
+} from "@mui/material";
 import FilmListObject from "./FilmListObject";
 import Grid from "@mui/material/Unstable_Grid2";
 import Film from "./Film";
+import MenuItem from "@mui/material/MenuItem";
 
 const FilmList = () => {
     const [films, setFilms] = React.useState<Array<Film>>([])
     const [genres, setGenres] = React.useState < Array < Genre >> ([])
+    const [filterGenres, setFilterGenres] = React.useState < Array < string >> ([])
     const [errorFlag, setErrorFlag] = React.useState(false);
     const [errorMessage, setErrorMessage] = React.useState("")
     const [page, setPage] = useState(1);
     const filmsPerPage = 10;
     const pageCount = Math.ceil(films.length / filmsPerPage)
 
+
+
     React.useEffect(() => {
         getGenres()
         getFilms()
-    }, [setGenres, setFilms])
+    }, [])
+
+
+    React.useEffect(() => {
+        getFilms()
+    }, [filterGenres])
+
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+            },
+        },
+    };
+
+
     const getGenres = () => {
         axios.get('http://localhost:4941/api/v1/films/genres')
             .then((response) => {
@@ -31,7 +60,9 @@ const FilmList = () => {
     }
 
     const getFilms = () => {
-        axios.get('http://localhost:4941/api/v1/films')
+        axios.get('http://localhost:4941/api/v1/films', {params: {
+                genreIds: filterGenres
+            }})
             .then((response) => {
                 setErrorFlag(false)
                 setErrorMessage("")
@@ -40,17 +71,56 @@ const FilmList = () => {
                 setErrorFlag(true)
                 setErrorMessage(error.toString())
             })
+        console.log("Genres", filterGenres)
+        console.log("Films", films)
     }
 
     const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
         setPage(value);
     }
-    const film_rows = () => films.slice((page - 1) * filmsPerPage, page * filmsPerPage).map((film: Film) => <Grid><FilmListObject key={film.filmId + film.title} film={film} genres={genres} /></Grid>)
+    const film_rows = () => films.slice((page - 1) * filmsPerPage, page * filmsPerPage).map((film: Film) => <Grid key={film.filmId}><FilmListObject film={film} genres={genres} /></Grid>)
+
+    const handleChange = (event: SelectChangeEvent<typeof filterGenres>) => {
+        const {
+            target: { value },
+        } = event;
+        setFilterGenres(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+        console.log(value)
+    };
+
 
     return (
         <Grid container rowSpacing={2}>
             <Grid xs={12} display="flex" justifyContent="center" alignItems="center">
                 <h1>Films</h1>
+            </Grid>
+            <Grid xs={12} display="flex" justifyContent="center" alignItems="center">
+                <div>
+                    <FormControl sx={{ m: 1, width: 300 }}>
+                        <InputLabel id="multiple-genre-label">Genre</InputLabel>
+                        <Select
+                            labelId="multiple-genre-label"
+                            id="multiple-genre"
+                            multiple
+                            value={filterGenres}
+                            onChange={handleChange}
+                            input={<OutlinedInput label="Genres" />}
+                            MenuProps={MenuProps}
+                        >
+                            {genres.map((genre) => (
+                                <MenuItem
+                                    key={genre.genreId}
+                                    value={genre.genreId}
+                                >
+                                    {genre.name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </div>
             </Grid>
             <Grid xs={12} display="flex" justifyContent="center" alignItems="center">
                 <Grid container spacing={6} display="flex" justifyContent="center" alignItems="center" disableEqualOverflow>
