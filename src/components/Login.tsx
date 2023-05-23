97119f569c358747fd74e9db8141bb3ee674abb3
Sean Marriott <sma462@uclive.ac.nz>
2023-05-23 12:04:54 +1200

    Added login form

---
+++ src/components/Login.tsx
@@ -0,0 +1,121 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from "@mui/material/Unstable_Grid2";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import axios from "axios";
+
+const Login = () => {
+    const [errorFlag, setErrorFlag] = React.useState(false)
+    const [errorMessage, setErrorMessage] = React.useState("")
+    const [authToken, setAuthToken] = React.useState("")
+    const [userId, setUserId] = React.useState(-1)
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const submitData = JSON.stringify({
+            "email": data.get('email'),
+            "password": data.get('password'),
+        })
+        axios.post('http://localhost:4941/api/v1/users/login', submitData, {headers: {'Content-Type': 'application/json'}})
+            .then((response) => {
+                setErrorFlag(false)
+                setErrorMessage("")
+                setAuthToken(response.data.token)
+                setUserId(response.data.userId)
+            }, (error) => {
+                setErrorFlag(true)
+                setErrorMessage(error.toString())
+            })
+    };
+
+
+
+    return (
+        <Grid container m={3} component={Paper} elevation={6} square>
+            <Grid
+                xs={false}
+                sm={4}
+                md={6}
+                sx={{
+                    backgroundImage: 'url(LoginImage.png)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            />
+            <Grid xs={12} sm={8} md={6}>
+                <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>)
+}
+
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import NotFound from "./components/NotFound";
 import Film from "./components/Film";
 import Navbar from "./components/Navbar";
 import FilmList from "./components/FilmList";
+import Login from "./components/Login";
 
 
 const root = ReactDOM.createRoot(
@@ -17,6 +18,7 @@ root.render(
         <Router>
             <div>
                 <Routes>
+                    <Route path="/login" element={<Login/>}></Route>
                     <Route path="/films" element={<FilmList/>}/>
                     <Route path="/film/:id" element={<Film/>}/>
                     <Route path="*" element={<NotFound/>}/>
