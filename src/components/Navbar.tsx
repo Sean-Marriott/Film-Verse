dd723e92f56a5063ab034dd974298598e5828cdb
Sean Marriott <sma462@uclive.ac.nz>
2023-05-26 14:35:22 +1200

    Added a navigation bar functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,9 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import {Alert, Link, Snackbar} from "@mui/material";
 import {useQuery, useQueryClient} from "react-query";
@@ -18,6 +14,7 @@ import {logout} from "../api/usersApi";
 import {useNavigate} from "react-router-dom";
 import {AxiosError} from "axios";
 import {useUserStore} from "../store";
+import TemporaryDrawer from "./TemporaryDrawer";
 
 
 const Navbar = () => {
@@ -140,26 +137,6 @@ const Navbar = () => {
             open={isMobileMenuOpen}
             onClose={handleMobileMenuClose}
         >
-            <MenuItem>
-                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={4} color="error">
-                        <MailIcon />
-                    </Badge>
-                </IconButton>
-                <p>Messages</p>
-            </MenuItem>
-            <MenuItem>
-                <IconButton
-                    size="large"
-                    aria-label="show 17 new notifications"
-                    color="inherit"
-                >
-                    <Badge badgeContent={17} color="error">
-                        <NotificationsIcon />
-                    </Badge>
-                </IconButton>
-                <p>Notifications</p>
-            </MenuItem>
             <MenuItem onClick={handleProfileMenuOpen}>
                 <IconButton
                     size="large"
@@ -192,15 +169,7 @@ const Navbar = () => {
 
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+                    <TemporaryDrawer/>
                     <Link href="/films">
                         <Logo />
                     </Link>
@@ -220,20 +189,6 @@ const Navbar = () => {
                     </Typography>
                     <Box sx={{ flexGrow: 1 }} />
                     <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                            <Badge badgeContent={4} color="error">
-                                <MailIcon />
-                            </Badge>
-                        </IconButton>
-                        <IconButton
-                            size="large"
-                            aria-label="show 17 new notifications"
-                            color="inherit"
-                        >
-                            <Badge badgeContent={17} color="error">
-                                <NotificationsIcon />
-                            </Badge>
-                        </IconButton>
                         <IconButton
                             size="large"
                             edge="end"
---
+++ src/components/TemporaryDrawer.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
+import {useUserStore} from "../store";
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import TheatersIcon from '@mui/icons-material/Theaters';
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+export default function TemporaryDrawer() {
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer =
+        (anchor: Anchor, open: boolean) =>
+            (event: React.KeyboardEvent | React.MouseEvent) => {
+                if (
+                    event.type === 'keydown' &&
+                    ((event as React.KeyboardEvent).key === 'Tab' ||
+                        (event as React.KeyboardEvent).key === 'Shift')
+                ) {
+                    return;
+                }
+
+                setState({ ...state, [anchor]: open });
+            };
+
+    const list = (anchor: Anchor) => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton href="/films">
+                        <ListItemIcon>
+                            <TheatersIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="All Films" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton href="/addFilm">
+                        <ListItemIcon>
+                            <AddBoxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Add Film" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
+
+    return (
+        <React.Fragment>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+                onClick={toggleDrawer('left', true)}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Drawer
+                anchor='left'
+                open={state['left']}
+                onClose={toggleDrawer('left', false)}
+            >
+                {list('left')}
+            </Drawer>
+        </React.Fragment>
+    );
+}
